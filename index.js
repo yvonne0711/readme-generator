79a2c742edf9616e2aad39b892f3c5a102e89071
yvonne <yvonnewong1107@gmail.com>
2023-12-15 20:14:45 +0000

    array of questions with inquirer prompts

--- index.js
+++ index.js
@@ -4,9 +4,57 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = () =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Write a description of your project:',
+                name: 'description',
+            },
+            {
+                type: 'checkbox',
+                message: 'What sections do you need for your README.md?',
+                name: 'tableOfContents',
+                choices: ['Installation', 'Usage', 'Licence', 'Contributing', 'Tests', 'Questions']
+            },
+            {
+                type: 'input',
+                message: 'What are the steps required for installing your project?',
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Provide instructions and examples for use:',
+                name: 'usage',
+            },
+            {
+                type: 'list',
+                message: 'Which licence do you need for your project?',
+                name: 'licence',
+                choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            },
+            {
+                type: 'input',
+                message: 'Provide guidelines on how to contribute:',
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: 'Provide examples on how to run tests for your project:',
+                name: 'tests',
+            },
+            {
+                type: 'input',
+                message: 'Additional information:',
+                name: 'questions',
+            },
+        ]);
 
 // function to write README file
 function writeToFile(fileName, data) {
